56846365d36f64d30b700c1dcef675f39b2e2df7
kevin kennedy <kjkennedy96@gmail.com>
2023-02-18 01:30:12 +0000

    Completed Challenge Is Sorted

--- src/main/java/IsSorted.java
+++ src/main/java/IsSorted.java
@@ -1,3 +1,4 @@
+
 public class IsSorted {
     /**
      * Return true if arr is sorted, smallest to largest, false otherwise.
@@ -7,6 +8,10 @@ public class IsSorted {
      * @return true if arr is sorted (all the numbers are in order). false if arr is not sorted.
      */
     public boolean checkSort(int[] arr){
-        return false;
+        for (int i = 1; i < arr.length; i++){
+        if (arr[i - 1] > arr[i])
+            return false;
+        }
+        return true;
     }
 }
